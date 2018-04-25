542598e0fcca05c4943f9904e1dd307feb4e6120
a_tretyakov <artem.tretyakov.91@inbox.ru>
2018-04-25 18:06:19 +0300

    fix

--- app/Http/Controllers/API/V1/ArmyController.php
+++ app/Http/Controllers/API/V1/ArmyController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\Archer;
 use App\Http\Controllers\Controller;
+use App\Models\Units\Army;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Response;
 
@@ -19,4 +21,15 @@ class ArmyController extends Controller
     }
 
 
+    public function take($id)
+    {
+        $unit = Archer::find($id);
+        $army = Army::find(1);
+
+        $army->addUnit($unit);
+        $unit->occupated_id = $army->id;
+        $unit->save();
+
+        $army->strength = $army->bombardStrength();
+    }
 }
